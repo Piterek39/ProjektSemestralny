77af9f4eb1a5857efd697cbdd617968d343ad38e
Piotr <piterek39@gmail.com>
2023-06-13 20:51:40 +0200

    views

--- ProjektSemestralny/MainWindow.xaml.cs
+++ ProjektSemestralny/MainWindow.xaml.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿using ProjektSemestralny.Classes;
+using ProjektSemestralny.Models;
+using ProjektSemestralny.Views;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +28,29 @@ namespace ProjektSemestralny
         public MainWindow()
         {
             InitializeComponent();
+            var page = new HomeView();
+            mainFrame.Content = page;
         }
+        private void HotelButton_Click(object sender, RoutedEventArgs e)
+        {
+            var page = new Hotels();
+            mainFrame.Content = page;
+        }
+        private void CustomerButton_Click(object sender, RoutedEventArgs e)
+        {
+            var page = new Customers();
+            mainFrame.Content = page;
+        }
+        private void RoomButton_Click(object sender, RoutedEventArgs e)
+        {
+            var page = new Rooms();
+            mainFrame.Content = page;
+        }
+        private void ReservationButton_Click(object sender, RoutedEventArgs e)
+        {
+            var page = new Reservations();
+            mainFrame.Content = page;
+        }
+
     }
 }
---
+++ ProjektSemestralny/Views/Hotels.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProjektSemestralny.Views
+{
+    /// <summary>
+    /// Interaction logic for Hotel.xaml
+    /// </summary>
+    public partial class Hotels : Page
+    {
+        public Hotels()
+        {
+            InitializeComponent();
+        }
+    }
+}
